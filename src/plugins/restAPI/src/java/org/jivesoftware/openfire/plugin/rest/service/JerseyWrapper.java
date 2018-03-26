408c0bca56c004045fc875eafd79104b37f95738
Cai Jiang <luffy.ja@gmail.com>
2018-03-26 14:23:16 +0800

    add ValidateCodeService

--- src/plugins/restAPI/src/java/org/jivesoftware/openfire/plugin/rest/service/JerseyWrapper.java
+++ src/plugins/restAPI/src/java/org/jivesoftware/openfire/plugin/rest/service/JerseyWrapper.java
@@ -22,22 +22,22 @@ import com.sun.jersey.spi.container.servlet.ServletContainer;
  * The Class JerseyWrapper.
  */
 public class JerseyWrapper extends ServletContainer {
-	
+
 	/** The Constant CUSTOM_AUTH_PROPERTY_NAME */
 	private static final String CUSTOM_AUTH_PROPERTY_NAME = "plugin.restapi.customAuthFilter";
-	
+
 	/** The Constant REST_AUTH_TYPE */
 	private static final String REST_AUTH_TYPE  = "plugin.restapi.httpAuth";
 
 	/** The Constant AUTHFILTER. */
 	private static final String AUTHFILTER = "org.jivesoftware.openfire.plugin.rest.AuthFilter";
-	
+
 	/** The Constant CORSFILTER. */
 	private static final String CORSFILTER = "org.jivesoftware.openfire.plugin.rest.CORSFilter";
 
 	/** The Constant CONTAINER_REQUEST_FILTERS. */
 	private static final String CONTAINER_REQUEST_FILTERS = "com.sun.jersey.spi.container.ContainerRequestFilters";
-	
+
 	/** The Constant CONTAINER_RESPONSE_FILTERS. */
 	private static final String CONTAINER_RESPONSE_FILTERS = "com.sun.jersey.spi.container.ContainerResponseFilters";
 
@@ -61,29 +61,29 @@ public class JerseyWrapper extends ServletContainer {
 
 	/** The prc. */
 	private static PackagesResourceConfig prc;
-	
+
 	/** The Constant JERSEY_LOGGER. */
 	private final static Logger JERSEY_LOGGER = Logger.getLogger("com.sun.jersey");
-	
+
 	private static String loadingStatusMessage = null;
-	
+
 	static {
 		JERSEY_LOGGER.setLevel(Level.SEVERE);
 		config = new HashMap<String, Object>();
 		config.put(RESOURCE_CONFIG_CLASS_KEY, RESOURCE_CONFIG_CLASS);
 		prc = new PackagesResourceConfig(SCAN_PACKAGE_DEFAULT);
-		prc.setPropertiesAndFeatures(config);		
+		prc.setPropertiesAndFeatures(config);
 		prc.getProperties().put(CONTAINER_RESPONSE_FILTERS, CORSFILTER);
 		loadAuthenticationFilter();
 
 		prc.getClasses().add(RestAPIService.class);
-		
+
 		prc.getClasses().add(MUCRoomService.class);
 		prc.getClasses().add(MUCRoomOwnersService.class);
 		prc.getClasses().add(MUCRoomAdminsService.class);
 		prc.getClasses().add(MUCRoomMembersService.class);
 		prc.getClasses().add(MUCRoomOutcastsService.class);
-		
+
 		prc.getClasses().add(UserServiceLegacy.class);
 		prc.getClasses().add(UserService.class);
 		prc.getClasses().add(UserRosterService.class);
@@ -98,6 +98,7 @@ public class JerseyWrapper extends ServletContainer {
 		prc.getClasses().add(RESTExceptionMapper.class);
 
 		prc.getClasses().add(RongCloudService.class);
+		prc.getClasses().add(ValidateCodeService.class);
 	}
 
 	public static String tryLoadingAuthenticationFilter(String customAuthFilterClassName) {
@@ -110,18 +111,18 @@ public class JerseyWrapper extends ServletContainer {
 		} catch (ClassNotFoundException e) {
 			loadingStatusMessage = "No custom auth filter found for restAPI plugin with name " + customAuthFilterClassName;
         }
-		
+
 		if(customAuthFilterClassName == null || customAuthFilterClassName.isEmpty())
 			loadingStatusMessage = "Classname field can't be empty!";
 		return loadingStatusMessage;
 	}
-	
+
 	public static String loadAuthenticationFilter() {
 		// Check if custom AuthFilter is available
 		String customAuthFilterClassName = JiveGlobals.getProperty(CUSTOM_AUTH_PROPERTY_NAME);
 		String restAuthType = JiveGlobals.getProperty(REST_AUTH_TYPE);
 		String pickedAuthFilter = AUTHFILTER;
-		
+
 		try {
 			if(customAuthFilterClassName != null && "custom".equals(restAuthType)) {
 				Class.forName(customAuthFilterClassName, false, JerseyWrapper.class.getClassLoader());
@@ -131,22 +132,22 @@ public class JerseyWrapper extends ServletContainer {
 		} catch (ClassNotFoundException e) {
 			loadingStatusMessage = "No custom auth filter found for restAPI plugin! " + customAuthFilterClassName + " " + restAuthType;
         }
-		
-		prc.getProperties().put(CONTAINER_REQUEST_FILTERS, pickedAuthFilter);	
+
+		prc.getProperties().put(CONTAINER_REQUEST_FILTERS, pickedAuthFilter);
 		return loadingStatusMessage;
 	}
-	
+
 	/**
 	 * Instantiates a new jersey wrapper.
 	 */
 	public JerseyWrapper() {
 		super(prc);
-		
+
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
 	 */
 	@Override
@@ -159,7 +160,7 @@ public class JerseyWrapper extends ServletContainer {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see com.sun.jersey.spi.container.servlet.ServletContainer#destroy()
 	 */
 	@Override
@@ -168,7 +169,7 @@ public class JerseyWrapper extends ServletContainer {
 		// Release the excluded URL
 		AuthCheckFilter.removeExclude(SERVLET_URL);
 	}
-	
+
 	/*
 	 * Returns the loading status message.
 	 *
@@ -177,5 +178,5 @@ public class JerseyWrapper extends ServletContainer {
 	public static String getLoadingStatusMessage() {
 		return loadingStatusMessage;
 	}
-	
+
 }
